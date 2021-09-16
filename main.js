d8c0f8f5362240dc9e659a00b57a9f8f263af407
Devin Shank <Drinks4Gamers@gmail.com>
2021-09-16 10:32:05 -0400

    Switched to instance mode

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var lastPosY;
 
 let backgroundColor = 230;
 let penColor = 20;
+let penSize = 20;
 
 //Used to tell if the user is drawing or not, if not, then reset that last point so it doesnt draw a line in a random direction
 var drawing;
@@ -10,45 +11,45 @@ var drawing;
 function setup() {
   createCanvas(windowWidth - 10, windowHeight - 200);
   background(backgroundColor);
-  
+
   drawing = false;
 }
 
 function draw() {
-  if (mouseIsPressed && drawing == true){
+  if (mouseIsPressed && drawing == true) {
     //Is always erasing
     let strokeColor = backgroundColor;
-    if (mouseButton === LEFT){
+    if (mouseButton === LEFT) {
       strokeColor = penColor
     }
-    strokeWeight(20);
+    strokeWeight(penSize);
     stroke(strokeColor);
     line(mouseX, mouseY, lastPosX, lastPosY);
 
     noStroke();
     fill(strokeColor);
-    ellipse(mouseX, mouseY, 20, 20)
+    ellipse(mouseX, mouseY, penSize, penSize)
 
     lastPosX = mouseX;
     lastPosY = mouseY;
-      
+
     //Script ends here if you are drawing
-      return;
+    return;
   }
-  if (mouseIsPressed && drawing == false){
+  if (mouseIsPressed && drawing == false) {
     drawing = true;
-    
+
     lastPosX = mouseX;
     lastPosY = mouseY;
   }
 }
 
 
-function mouseReleased(){
+function mouseReleased() {
   drawing = false;
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth - 10 , windowHeight - 200);
+  resizeCanvas(windowWidth - 10, windowHeight - 200);
   background(backgroundColor);
 }
